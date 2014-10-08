d0fe081367885f4ad1079a05cb702e2623ebfa12
Rakesh Kumar Shardiwal <rakesh.shardiwal@gmail.com>
2014-10-08 23:53:45 +0530

    More Locale Translation Added ar - Arabic de - German es - Spanish fr - French hi - Hindi id - Indonesian ja - Japanese ms - Malay pl - Polish pt - Portuguese ru - Russian zh_cn - Chinese - China

--- lib/Time/Duration/Concise/Locale/id.pm
+++ lib/Time/Duration/Concise/Locale/id.pm
@@ -1,4 +1,4 @@
-package Time::Duration::Concise::Locale::ms;
+package Time::Duration::Concise::Locale::id;
 
 use 5.006;
 use strict;
@@ -8,11 +8,11 @@ our $VERSION = '0.1';
 
 =head1 NAME
 
-Time::Duration::Concise::Locale::ms - Malay - Malaysia locale translation.
+Time::Duration::Concise::Locale::id - Indonesian locale translation.
 
 =head1 DESCRIPTION
 
-Time::Duration::Concise uses Time::Duration::Concise::Locale::ms to localize concise time duration string representation.
+Time::Duration::Concise uses Time::Duration::Concise::Locale::id to localize concise time duration string representation.
 
 =head1 VERSION
 
@@ -29,17 +29,17 @@ Localized translation hash
 sub translation {
     my ( $self ) = @_;
     return {
-	'second'  => 'kedua',
-	'seconds' => 'saat',
-	'minute'  => 'minit',
-	'minutes' => 'minit',
+	'second'  => 'detik', 
+	'seconds' => 'detik',
+	'minute'  => 'menit',
+	'minutes' => 'menit',
 	'hour'    => 'jam',
 	'hours'   => 'jam',
     'day'     => 'hari',
 	'days'    => 'hari',
 	'month'   => 'bulan',
 	'months'  => 'bulan',
-	'year'    => 'tahun'
+	'year'    => 'tahun',
     };
 }
 
@@ -49,4 +49,4 @@ Binary.com, C<< <perl at binary.com> >>
 
 =cut
 
-1;    # End of Time::Duration::Concise::Locale::ms
+1;    # End of Time::Duration::Concise::Locale::id
